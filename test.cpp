a8ddd21f51ffd342c4104e123b9901606bea41ca
Arton Chang <arton0306@gmail.com>
2015-03-04 03:08:59 -0800

    rename

--- test.cpp
+++ test.cpp
@@ -1,45 +1,44 @@
 #include <sys/time.h>
 #include <iostream>
 
-#define GU_AVOID_LOCK_NUM 1000
-static long long guTimecost[GU_AVOID_LOCK_NUM] = {0}; // avoid lock
+#define ar_AVOID_LOCK_NUM 1000
+static long long ar_TimeCost[ar_AVOID_LOCK_NUM] = {0}; // avoid lock
+static long long ar_getTimeCost() { 
+    long long ret = 0;
+    for ( int i = 0; i < ar_AVOID_LOCK_NUM; ++i )
+        ret+=ar_TimeCost[i];
+    return ret;
+}
+static void ar_resetTimeCost() {
+    for ( int i = 0; i < ar_AVOID_LOCK_NUM; ++i )
+        ar_TimeCost[i]=0;
+}
 
-#define PROFILE_START \
+#define ar_PROFILE_START \
     struct timeval tp; \
     gettimeofday(&tp,NULL); \
     long long watchTimestamp = tp.tv_sec * 1000 + tp.tv_usec / 1000; \
     cout << "start at: " << watchTimestamp << endl;
 
-#define PROFILE_STOP \
-    int randint = tp.tv_usec % GU_AVOID_LOCK_NUM; \
+#define ar_PROFILE_STOP \
+    int randint = tp.tv_usec % ar_AVOID_LOCK_NUM; \
     gettimeofday(&tp,NULL); \
     long long watchTimestamp2 = tp.tv_sec * 1000 + tp.tv_usec / 1000; \
     cout << "stop at: " << watchTimestamp2 << endl; \
-    guTimecost[randint] += watchTimestamp2 - watchTimestamp;
-
-long long getGuTimeCost() {
-    long long ret = 0;
-    for ( int i = 0; i < GU_AVOID_LOCK_NUM; ++i )
-        ret+=guTimecost[i];
-    return ret;
-}
-void resetGuTimeCost() {
-    for ( int i = 0; i < GU_AVOID_LOCK_NUM; ++i )
-        guTimecost[i]=0;
-}
+    ar_TimeCost[randint] += watchTimestamp2 - watchTimestamp;
  
 using namespace std;
 int main() {
-    PROFILE_START;
+    ar_PROFILE_START;
     int x = 0;
     cin >> x;
     int sum = 0;
     for ( int i = 0; i < x && x > 0; ++i )
         sum += i;
     cout << sum << endl;
-    PROFILE_STOP;
+    ar_PROFILE_STOP;
 
-    cout << getGuTimeCost() << endl;
+    cout << ar_getTimeCost() << endl;
     
     return 0;
 }
