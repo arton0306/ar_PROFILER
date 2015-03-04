112005350d73c75fd927b08e5f2ece0b47cf13b5
Arton Chang <arton0306@gmail.com>
2015-03-04 03:18:37 -0800

    us precision

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 
 #define ar_AVOID_LOCK_NUM 1000
 static long long ar_TimeCost[ar_AVOID_LOCK_NUM] = {0}; // avoid lock
-static long long ar_getTimeCost() { 
+static long long ar_getTimeCostUs() { 
     long long ret = 0;
     for ( int i = 0; i < ar_AVOID_LOCK_NUM; ++i )
         ret+=ar_TimeCost[i];
@@ -14,19 +14,33 @@ static void ar_resetTimeCost() {
         ar_TimeCost[i]=0;
 }
 
-#define ar_PROFILE_START \
-    struct timeval tp; \
-    gettimeofday(&tp,NULL); \
-    long long watchTimestamp = tp.tv_sec * 1000 + tp.tv_usec / 1000; \
-    cout << "start at: " << watchTimestamp << endl;
+// #define ar_DUMP
+#ifdef ar_DUMP
+    #define ar_PROFILE_START \
+        struct timeval tp; \
+        gettimeofday(&tp,NULL); \
+        long long t1 = tp.tv_sec * (long long)1000000 + tp.tv_usec; \
+        cout << "start at: " << t1 << endl;
+    
+    #define ar_PROFILE_STOP \
+        int randint = tp.tv_usec % ar_AVOID_LOCK_NUM; \
+        gettimeofday(&tp,NULL); \
+        long long t2 = tp.tv_sec * (long long)1000000 + tp.tv_usec; \
+        cout << "stop at: " << t2 << endl; \
+        ar_TimeCost[randint] += t2 - t1;
+#else
+    #define ar_PROFILE_START \
+        struct timeval tp; \
+        gettimeofday(&tp,NULL); \
+        long long t1 = tp.tv_sec * (long long)1000000 + tp.tv_usec; \
+    
+    #define ar_PROFILE_STOP \
+        int randint = tp.tv_usec % ar_AVOID_LOCK_NUM; \
+        gettimeofday(&tp,NULL); \
+        long long t2 = tp.tv_sec * (long long)1000000 + tp.tv_usec; \
+        ar_TimeCost[randint] += t2 - t1;
+#endif
 
-#define ar_PROFILE_STOP \
-    int randint = tp.tv_usec % ar_AVOID_LOCK_NUM; \
-    gettimeofday(&tp,NULL); \
-    long long watchTimestamp2 = tp.tv_sec * 1000 + tp.tv_usec / 1000; \
-    cout << "stop at: " << watchTimestamp2 << endl; \
-    ar_TimeCost[randint] += watchTimestamp2 - watchTimestamp;
- 
 using namespace std;
 int main() {
     ar_PROFILE_START;
@@ -38,7 +52,7 @@ int main() {
     cout << sum << endl;
     ar_PROFILE_STOP;
 
-    cout << ar_getTimeCost() << endl;
+    cout << ar_getTimeCostUs() << endl;
     
     return 0;
 }
